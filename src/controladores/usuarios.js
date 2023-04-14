ee1a697f894056899189ff102d5759e49358b564
Douglas Santana Costa <douglas.santana1777@gmail.com>
2023-04-14 17:18:40 -0300

    :sparkles: feat: Cadastrar usuário e Login do usuário

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,64 @@
+const knex = require("../conexão");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const rotasUsuario = {
+  async cadastrarUsuario(req, res) {
+    const { nome, email, senha } = req.body;
+    try {
+      const criptografiSenha = await bcrypt.hash(senha, 10);
+      const usuarioCadastrado = await knex("usuarios")
+        .insert({ nome, email, criptografiSenha })
+        .returning("*");
+
+      const usuarioSemSenha = {
+        id: usuarioCadastrado.rows[0].id,
+        nome: usuarioCadastrado.rows[0].nome,
+        email: usuarioCadastrado.rows[0].email,
+      };
+
+      return res.status(201).json(usuarioSemSenha);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  },
+  async loginUsuario(req, res) {
+    const { email, senha } = req.body;
+
+    try {
+      const consultaUSuario = await knex("usuarios").where(email);
+
+      if (!consultaUSuario.rows[0]) {
+        return res.status(400).json({ message: "usuario ou senha invalido" });
+      }
+
+      const validarLogin = await bcrypt.compare(
+        senha,
+        consultaUSuario.rows[0].senha
+      );
+
+      if (!validarLogin) {
+        return res.status(400).json({ message: "usuario ou senha invalido" });
+      }
+
+      const token = await jwt.sign(
+        { id: consultaUSuario.rows[0].id },
+        process.env.SECRETJWT,
+        { expiresIn: "24h" }
+      );
+
+      return res.status(200).json({
+        usuario: {
+          id: consultaUSuario.rows[0].id,
+          nome: consultaUSuario.rows[0].nome,
+          email,
+        },
+        token,
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  },
+};
+module.exports = rotasUsuario;
