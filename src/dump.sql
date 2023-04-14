a9260fecedd02b6dc1a65069395a1a80d490fa4a
hyan Figueiredo <hyansantana10@gmail.com>
2023-04-14 16:20:42 -0300

    criação e configuração inicial do projeto

---
+++ src/dump.sql
@@ -0,0 +1,23 @@
+create database PDV;
+
+create table usuarios(
+id serial primary key,
+  nome text not null,
+  email text unique not null,
+  senha text not null
+);
+
+create table categorias (
+id serial primary key,
+  descricao text not null
+);
+
+insert into categorias (descricao) values ('Informática');
+insert into categorias (descricao) values ('Celulares');
+insert into categorias (descricao) values ('Beleza e Perfumaria');
+insert into categorias (descricao) values ('Mercado');
+insert into categorias (descricao) values ('Livros e Papelaria');
+insert into categorias (descricao) values ('Brinquedos');
+insert into categorias (descricao) values ('Moda');
+insert into categorias (descricao) values ('Bebê');
+insert into categorias (descricao) values ('Games');
