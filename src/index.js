4d90bde0aa20ca8756c79c7071765f1b7927debf
Douglas Santana Costa <douglas.santana1777@gmail.com>
2023-04-15 16:23:15 -0300

    :sparkles: feat: Autenticação, login e rotas do login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const rotasUsuarios = require('./rotas/usuarios.js');
-const rotasCategorias = require('./rotas/categorias');
+const rotasUsuarios = require("./rotas/usuarios.js");
+const rotasCategorias = require("./rotas/categorias");
+const rotasAutenticacao = require("./rotas/autenticacao");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/usuarios', rotasUsuarios);
-app.use('/categorias', rotasCategorias);
-
+app.use("/usuarios", rotasUsuarios);
+app.use("/categorias", rotasCategorias);
+app.use("/login", rotasAutenticacao);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Online on Port ${process.env.PORT}`);
-})
+  console.log(`Online on Port ${process.env.PORT}`);
+});
--- src/rotas/usuarios.js
+++ src/rotas/usuarios.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
-const { cadastrarUsuario, login } = require("../controladores/usuarios");
+const cadastrarUsuario = require("../controladores/usuarios");
 
 const router = express.Router();
 
 router.post("/", cadastrarUsuario);
-router.post("/", login);
 
 module.exports = router;
