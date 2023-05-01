0d486b90db7b663ade8270c4f2310f127edb455a
Bruno <brunoornelio@hotmail.com>
2023-05-01 14:48:34 -0300

    test/estruturando testes

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,4 +1,24 @@
-const { teste, producao } = require('./knex/variaveis')
+
+const teste = {
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: process.env.DATABASE_USER_TEST,
+    password: process.env.DATABASE_USER_PASSWORD,
+    database: 'pdv',
+  },
+}
+
+const producao = {
+  client: 'pg',
+  connection: {
+    host: process.env.HOST_BD,
+    port: process.env.PORT_BD,
+    user: process.env.USER_BD,
+    password: process.env.PASSWORD_BD,
+    database: process.env.DATABASE_BD,
+  },
+}
 
 const pegaVariavel = () => {
   if (process.env.NODE_ENV) {
