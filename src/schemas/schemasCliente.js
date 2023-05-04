4bf42bffded2610432719680b3a56f9a4fd34e5b
Bruno <brunoornelio@hotmail.com>
2023-05-03 22:35:17 -0300

    fixed/correção de erros de sintaxe

--- src/schemas/schemasCliente.js
+++ src/schemas/schemasCliente.js
@@ -5,15 +5,15 @@ const validate = require('../intermediarios/validacaoYup')
 const validacaoCamposCliente = validate((schemas) => {
   return schemas(
     yup.object({
-      nome: yup.string().trim().required(),
+      nome: yup.string().strict(true).trim().required(),
       email: yup.string().email().lowercase().trim().required(),
-      cpf: yup.number().positive().min(11).required(),
-      cep: yup.number().min(8),
-      rua: yup.string(),
-      numero: yup.number().positive(),
-      bairro: yup.string(),
-      cidade: yup.string(),
-      estado: yup.string(),
+      cpf: yup.number().positive().integer().min(11).required(),
+      cep: yup.number().positive().integer().min(8),
+      rua: yup.string().strict(true),
+      numero: yup.number().positive().integer(),
+      bairro: yup.string().strict(true),
+      cidade: yup.string().strict(true),
+      estado: yup.string().strict(true),
     })
   )
 })
