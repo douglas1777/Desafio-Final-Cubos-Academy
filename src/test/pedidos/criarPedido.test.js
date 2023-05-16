24f6bcc36df2d02f1ad4cd1047b99b1bdfbecff7
hyan Figueiredo <hyansantana10@gmail.com>
2023-05-16 08:42:35 -0300

    test/criacção de testes de pedidos

---
+++ src/test/pedidos/criarPedido.test.js
@@ -0,0 +1,204 @@
+const { StatusCodes } = require('http-status-codes')
+const { testServer } = require('../jest.setup')
+
+let token
+const criaClienteRepetido = {
+    nome: 'José',
+    email: 'jose@email.com',
+    cpf: '12345668804',
+}
+
+describe('Criar cliente', () => {
+    beforeAll(async () => {
+        const { body } = await testServer
+            .post('/usuario')
+            .send({ nome: 'bruno', email: 'bruno@email.com', senha: '123' })
+
+        const logarUsuario = await testServer
+            .post('/login')
+            .send({ email: body.email, senha: '123' })
+
+        token = 'bearer ' + logarUsuario.body.token
+    })
+
+    it('deve recusar a criação de um pedido quando o id do cliente não é fornecido', async () => {
+        const pedido = {
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 1,
+                    quantidade_produto: 10,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toHaveProperty('cliente_id', 'cliente_id é um campo obrigatório');
+    });
+
+    it('deve recusar a criação de um pedido quando o id do produto não é fornecido', async () => {
+        const pedido = {
+            cliente_id: 1,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    quantidade_produto: 10,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toHaveProperty('produto_id', 'produto_id é um campo obrigatório');
+    });
+
+    it('deve recusar a criação de um pedido quando a quantidade do produto não é fornecida', async () => {
+        const pedido = {
+            cliente_id: 1,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 1,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toHaveProperty('quantidade_produto', 'quantidade_produto é um campo obrigatório')
+    })
+
+    it('deve recusar a criação de um pedido quando o cliente não é encontrado no banco de dados', async () => {
+        const pedido = {
+            cliente_id: 999,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 1,
+                    quantidade_produto: 10,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toEqual('Cliente não encontrado');
+    });
+
+    it('deve recusar a criação de um pedido quando o produto não é encontrado no banco de dados', async () => {
+        const pedido = {
+            cliente_id: 1,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 999,
+                    quantidade_produto: 10,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toEqual('Produto não encontrado');
+    });
+
+    it('deve recusar a criação de um pedido quando a quantidade de produto solicitada está acima do volume em estoque', async () => {
+        const pedido = {
+            cliente_id: 1,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 1,
+                    quantidade_produto: 100000,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(response.body).toHaveProperty('mensagem');
+        expect(response.body.mensagem).toEqual('A quantidade de produto solicitada está acima do volume em estoque');
+    });
+
+    it('deve criar um pedido com sucesso', async () => {
+        const pedido = {
+            cliente_id: 1,
+            observacao: 'Em caso de ausência recomendo deixar com algum vizinho',
+            pedido_produtos: [
+                {
+                    produto_id: 1,
+                    quantidade_produto: 10,
+                },
+                {
+                    produto_id: 2,
+                    quantidade_produto: 20,
+                },
+            ],
+        };
+
+        const response = await testServer
+            .post('/pedido')
+            .set('Authorization', token)
+            .send(pedido);
+
+        expect(response.statusCode).toEqual(StatusCodes.OK);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('cliente_id');
+        expect(response.body).toHaveProperty('observacao');
+        expect(response.body).toHaveProperty('valor_total');
+    });
+})
